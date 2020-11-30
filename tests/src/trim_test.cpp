384a732e92bd305d46cbdb972ebefae44d6570e5
James Smith <js7572305@gmail.com>
2020-11-30 12:37:24 +0300

    Added tests for trim function

---
+++ tests/src/trim_test.cpp
@@ -0,0 +1,12 @@
+#include "trim.h"
+#include <iostream>
+#include <string>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
+TEST_CASE("Trim test") {
+  CHECK(trim("  ad  ") == std::string("ad"));
+  CHECK(trim("  ad") == std::string("ad"));
+  CHECK(trim("ad  ") == std::string("ad"));
+}
